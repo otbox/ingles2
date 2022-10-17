c7a16e7dddfed049b9b329e19b53c896303944e4
otbox <otboxmarques88@gmail.com>
2022-10-17 18:25:32 -0300

    1.1
    adição do transcript, novo menu, palavras cruzadas, e reformulação do desingn da pagina

---
+++ ingles22/Name/quizmaavanca.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<head>
+	<meta charset=UTF-8" />
+	
+	<title>Advanced Name the Image</title>
+	
+	<link rel="stylesheet" type="text/css" href="../stylen.css" />
+</head>
+
+<body>
+<div class = 'geral'>
+    <div id="page-wrap">
+    <h1 class ='titulo'> Advanced <p>Name the Image </h1>
+
+    
+     <img  src="images/espumadeira.png" alt="skimmer"  width="300" height="300" class ='center-block'>         
+<h2 class="title">Name The Object:</h2>
+		
+		
+<form action="resultadoavanca.php" method="post" id="quiz">
+                 <div class="text">   
+                    <div >
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A" class='radiote'>A) Crayon  </label>
+                    
+                    
+                    
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B" class='radiote'>B) Wheelbarrow </label>
+                    
+                    
+                    
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C" class='radiote'>C) skimmer</label>
+                    
+                    
+                    
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D" class='radiote'>D) Plot </label>
+                    
+                 </div>
+                 
+                
+<!----------------------------------------------------------------QUESTAO 2-------------------------------------------------------------------------------->
+
+            <br>
+            <br>
+                    <img  src="images/concha.png" alt="Ladle" width="300" height="300" class ='center-block'>  
+                    <h2 class="title">Name The Object:</h2>
+                    <div class="text">  
+                    
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A" class='radiote'>A) Razor  </label>
+                    
+                    
+                    
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B" class='radiote'>B) Hook  </label>
+                    
+                    
+                    
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C" class='radiote'>C) Plier </label>
+                    
+                    
+                  
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                        <label for="question-2-answers-D" class='radiote'>D) Ladle </label>
+                    
+                    </div>
+                
+<!----------------------------------------------------------------------QUESTÂO 3------------------------------------------------------------------------------------------>
+                <br>
+                <br>
+                    <img  src="images/foice.png " alt="Sickle"width="300" height="300" class ='center-block'> 
+                    <h2 class="title">Name The Object:</h2>
+                    <div class="text"> 
+                    <br>
+                    
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A" class='radiote'>A) Sickle</label>
+                    
+
+                    
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B" class='radiote'>B) Socket wrench</label>
+                    
+
+                    
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C" class='radiote'>C) fan</label>
+                    
+                    
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                        <label for="question-3-answers-D" class='radiote'>D) Oven</label>
+                    
+                    </div>
+            <br><br><br>
+         
+                
+          
+                
+                
+            
+            <input class="submitbtn2"style='font-size:24px' type="submit" value="Show Result"/>
+</div>
+		</form>
+	
+	</div>
+    <br><br><br><br><br><br><br><br>
+
+</body>
+
+</html>
